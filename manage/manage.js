94cbdb94ef8a5ab4c154844806022f86bc0d6a10
tsbehlman <tsbehlman@gmail.com>
2015-04-16 05:22:56 -0400

    Huge code cleanup, multiple optimizations, fixed bugs, removal of obsolete code

--- manage/manage.js
+++ manage/manage.js
@@ -1,68 +1,90 @@
+if( window.safari === undefined ) {
+    window.safari = {
+        self: {
+            addEventListener: function() {},
+            tab: {
+                dispatchMessage: function( name, message ) {
+                    console.log( name + ": " + message );
+                }
+            }
+        }
+    }
+}
+
 var Manager = {
-    hideLabel: function(label) {
-        label.classList.remove('expanded');
-        label.classList.add('hidden');
-        label.nextElementSibling.style.display = 'none';
+    list: null,
+    form: null,
+    newLink: null,
+    includeLabel: null,
+    excludeLabel: null,
+    
+    current: null,
+    
+    hideLabel: function( label ) {
+        label.classList.add( 'hidden' );
     },
-    showLabel: function(label) {
-        label.classList.remove('hidden');
-        label.classList.add('expanded');
-        label.nextElementSibling.style.removeProperty('display');
+    showLabel: function( label ) {
+        label.classList.remove( 'hidden' );
     },
     clickLabel: function() {
-        if( this.classList.contains('expanded') ) {
-            Manager.hideLabel(this);
+        if( this.classList.contains( 'hidden' ) ) {
+            Manager.showLabel( this );
         }
         else {
-            Manager.showLabel(this);
+            Manager.hideLabel( this );
         }
     },
 
     // Style storage
-    createItems: function(){
-        safari.self.tab.dispatchMessage('items');
+    createItems: function() {
+        safari.self.tab.dispatchMessage( 'items' );
     },
 
-    removeItem: function(key){
-        safari.self.tab.dispatchMessage('removeItem', key);
+    removeItem: function( key ) {
+        safari.self.tab.dispatchMessage( 'removeItem', key );
     },
 
-    getItem: function(key){
-        safari.self.tab.dispatchMessage('getItem', key);
+    getItem: function( key ) {
+        safari.self.tab.dispatchMessage( 'getItem', key );
     },
 
-    setItem: function(key, values){
-        safari.self.tab.dispatchMessage('setItem', [key, values]);
-    },
-
-    // Convert to 'global' storage.
-    makeGlobal: function(){
-        safari.self.tab.dispatchMessage('shouldConvert');
+    setItem: function( key, values ) {
+        safari.self.tab.dispatchMessage( 'setItem', [ key, values ] );
     },
 
     // Manager application
-    start: function(){
-        const newLink = document.getElementById('new'),
-              form = document.getElementById('form');
-        Manager.makeGlobal();
+    start: function() {
+        Manager.list = document.getElementById( 'list' );
+        Manager.form = document.getElementById( 'form' );
+        Manager.newLink = document.getElementById( 'new' );
+        Manager.includeLabel = document.getElementById( 'label-includes' );
+        Manager.excludeLabel = document.getElementById( 'label-excludes' );
+        
         Manager.createItems();
 
         /* Events for the "New User CSS" sidebar link. */
-        newLink.addEventListener('click', function(event){
-            Manager.markCurrent(this);
-            Manager.bindNewForm();
-        }, false);
+        Manager.newLink.addEventListener( 'click', function( event ) {
+            if( this !== Manager.current ) {
+                Manager.markCurrent( this );
+                Manager.bindNewForm();
+            }
+        }, false );
 
-        document.getElementById('label-includes').addEventListener( 'click', Manager.clickLabel, false );
-        document.getElementById('label-excludes').addEventListener( 'click', Manager.clickLabel, false );
+        document.getElementById( 'label-includes' )
+            .addEventListener( 'click', Manager.clickLabel, false );
+        document.getElementById( 'label-excludes' )
+            .addEventListener( 'click', Manager.clickLabel, false );
 
         /* Defaults */
-        newLink.click();
+        Manager.newLink.click();
     },
 
-    $n: function(key) { return 'item-'+key; },
-    $p: function(key) { return document.getElementById(Manager.$n(key)); },
-    $a: function(key) { return Manager.$p(key).getElementsByTagName('a')[0]; },
+    getItemId: function( key ) {
+        return 'item-' + key;
+    },
+    getListItem: function( key ) {
+        return document.getElementById( Manager.getItemId( key ) );
+    },
 
     reloadStyles: function(){
         safari.self.tab.dispatchMessage('reloadStyles');
@@ -72,171 +94,157 @@ var Manager = {
         document.getElementById('title').textContent = title;
     },
 
-    setDomainsLabelState: function(includes, excludes){
-        const includeLabel = document.getElementById('label-includes'),
-              excludeLabel = document.getElementById('label-excludes');
-        (includes ? Manager.showLabel : Manager.hideLabel)(includeLabel);
-        (excludes ? Manager.showLabel : Manager.hideLabel)(excludeLabel);
+    setDomainsLabelState: function( includes, excludes ) {
+        Manager.showLabel( Manager.includeLabel );
+        ( excludes ? Manager.showLabel : Manager.hideLabel )( Manager.excludeLabel );
     },
 
-    createItem: function(key, data){
-        const list = document.getElementById('list');
-        var item = document.createElement('li'),
-            link = document.createElement('a'),
-            delete_link = document.createElement('a');
+    createItem: function( key, data ) {
+        var item = document.createElement( 'a' ),
+            delete_link = document.createElement( 'span' );
         
-        item.id = Manager.$n(key);
+        item.id = Manager.getItemId( key );
         
-        link.className = 'selector' + (data.enabled ? '' : ' disabled');
-        link.href = '#' + key;
-        link.textContent = data.name;
-        link.addEventListener( 'click', function(e) {
-            var key = this.hash.substr(1);
-            Manager.markCurrent(this);
-            Manager.getItem(key);
+        item.className = 'selector' + ( data.enabled ? '' : ' disabled' );
+        item.href = '#' + key;
+        item.textContent = data.name;
+        item.addEventListener( 'click', function( e ) {
+            if( this !== Manager.current ) {
+                var key = this.hash.substr( 1 );
+                Manager.markCurrent( this );
+                Manager.getItem( key );
+            }
         } );
         
         delete_link.className = 'delete';
-        delete_link.href = '#delete' + key;
-        delete_link.addEventListener( 'click', function(e) {
-            var key = this.hash.substr(7),
-                element = Manager.$p(key);
+        delete_link.addEventListener( 'click', function( e ) {
+            var element = this.parentNode,
+                key = element.hash.substr( 1 );
             
-            if (Manager.$a(key).hasClass('current')) {
-                var prev = element.getPrevious(),
-                    next = element.getNext(),
-                    target = document.getElementById('new');
-                if (next) {
-                    target = next.getElementsByTagName('a')[0];
-                } else if (prev) {
-                    target = prev.getElementsByTagName('a')[0];
-                }
-                target.click();
+            if( element === Manager.current ) {
+                (
+                    element.nextElementSibling ||
+                    element.previousElementSibling ||
+                    Manager.newLink
+                ).click();
             }
-            element.destroy();
-            Manager.removeItem(key);
+            
+            Manager.list.removeChild( element );
+            Manager.removeItem( key );
             Manager.reloadStyles();
+            e.stopPropagation();
         } );
         
-        item.appendChild(link);
-        item.appendChild(delete_link);
-        list.appendChild(item);
+        item.appendChild( delete_link );
+        Manager.list.appendChild( item );
         return item;
     },
 
-    markCurrent: function(element){
-        var current = document.getElementsByClassName('current')[0];
-        current && current.classList.remove('current');
-        if (typeof element === 'string')
-            element = Manager.$a(element);
-        element.classList.add('current');
-    },
-
-    constructDataFromForm: function(data, form){
-        data.name = form.name.value;
-        data.enabled = form.enabled.checked;
-        data.domains = form.domains.value.split('\n');
-        data.excludes = form.excludes.value.split('\n');
-        data.styles = form.styles.value;
-        data.script = form.script.value;
-        data.onload = form.onload.checked;
-        return data;
-    },
-
-    bindForm: function(data, fn){
-        const form = document.getElementById('form');
-        if (data.domains === undefined)  data.domains = [];
-        if (data.excludes === undefined) data.excludes = [];
-
-        form.name.value = data.name || '';
-        form.domains.value = data.domains.join('\n');
-        form.excludes.value = data.excludes.join('\n');
-        form.styles.value = data.styles;
-        form.script.value = data.script;
-        form.onload.checked = data.onload;
-        form.enabled.checked = data.enabled;
-
-        form.domains.value = data.domains.join('\n');
-        form.excludes.value = data.excludes.join('\n');
-        form.styles.value = data.styles === undefined ? null : data.styles;
-        form.script.value = data.script === undefined ? null : data.script;
-
-        /* Display */
-        Manager.setDomainsLabelState(data.domains.length, data.excludes.length);
-
-        form.__submitFn && form.removeEventListener('submit', form.__submitFn, false); // Cleanup
-        if (fn) {
-            form.__submitFn = function(event){
-                var formData = Manager.constructDataFromForm(data, this);
-                fn(formData);
-                
-                // Don't refresh the page
-                event.preventDefault();
-                return false;
-            };
-            form.addEventListener('submit', form.__submitFn, false);
+    markCurrent: function( element ) {
+        if( element !== Manager.current ) {
+            Manager.current && Manager.current.classList.remove( 'current' );
+            element.classList.add( 'current' );
+            Manager.current = element;
         }
     },
 
+    constructDataFromForm: function( data ) {
+        var includes = Manager.form.domains.value,
+            excludes = Manager.form.excludes.value;
+        data.name = Manager.form.name.value;
+        data.enabled = Manager.form.enabled.checked;
+        data.domains = includes.length ? includes.split( '\n' ) : [];
+        data.excludes = excludes.length ? excludes.split( '\n' ) : [];
+        data.styles = Manager.form.styles.value;
+        data.script = Manager.form.script.value;
+        data.onload = Manager.form.onload.checked;
+        return data;
+    },
+
+    bindForm: function( data, callback ) {
+        if( data.domains === undefined )  data.domains = [];
+        if( data.excludes === undefined ) data.excludes = [];
+
+        Manager.form.name.value = data.name || '';
+        Manager.form.domains.value = data.domains.join( '\n' );
+        Manager.form.excludes.value = data.excludes.join( '\n' );
+        Manager.form.styles.value = data.styles || '';
+        Manager.form.script.value = data.script || '';
+        Manager.form.onload.checked = data.onload;
+        Manager.form.enabled.checked = data.enabled;
+
+        /* Display */
+        Manager.setDomainsLabelState( data.domains.length, data.excludes.length );
+
+        Manager.form._submitCallback && Manager.form.removeEventListener( 'submit', Manager.form._submitCallback, false ); // Cleanup
+        
+        Manager.form._submitCallback = function( e ) {
+            callback( Manager.constructDataFromForm( data ) );
+            
+            // Don't refresh the page
+            event.preventDefault();
+            return false;
+        };
+        
+        Manager.form.addEventListener( 'submit', Manager.form._submitCallback, false );
+    },
+
     bindEditForm: function(key, data){
-        Manager.setTitle(data.name);
-        Manager.bindForm(data, function(formData){
-            Manager.setItem(key, formData);
-            Manager.reloadStyles();
-            // Always update display
-            var name = formData.name,
-                element = Manager.$a(key);
-            if (this.name.value)
-                name = this.name.value;
-            Manager.setTitle(name);
-            element.textContent = name;
-            element.classList.remove('disabled');
-            if (!formData.enabled)
-                element.classList.add('disabled');
+        Manager.setTitle( data.name );
+        Manager.bindForm( data, function( formData ) {
+            if( formData.name && ( formData.styles || formData.script ) && formData.domains ) {
+                Manager.setItem( key, formData );
+            
+                var item = Manager.getListItem( key );
+                // Always update display
+                Manager.setTitle( formData.name );
+                item.textContent = formData.name;
+                
+                if( formData.enabled ) {
+                    item.classList.remove( 'disabled' );
+                }
+                else {
+                    item.classList.add( 'disabled' );
+                }
+                
+                Manager.reloadStyles();
+            }
         });
     },
 
-    bindNewForm: function(){
-        Manager.setTitle('New Injection');
-        Manager.bindForm({}, function(formData){
-            var key = new Date().getTime();
-            if ((formData.styles || formData.script) && formData.domains) {
-                Manager.setItem(key, formData);
-                if (!formData.name)
-                    formData.name = key;
-                var item = Manager.createItem(key, formData);
-                item.getElementsByTagName('a')[0].click();
-                Manager.reloadStyles();
-                if (!formData.enabled)
+    bindNewForm: function() {
+        Manager.setTitle( 'New Injection' );
+        Manager.bindForm( { domains: [], excludes: [] }, function( formData ) {
+            var key = Date.now();
+            if( formData.name && ( formData.styles || formData.script ) && formData.domains ) {
+                Manager.setItem( key, formData );
+
+                var item = Manager.createItem( key, formData );
+                item.click();
+                
+                if( !formData.enabled ) {
                     item.classList.add('disabled');
+                }
+                
+                Manager.reloadStyles();
             }
         });
     }
 
 };
 
-function handleMessage(event) {
-    switch (event.name) {
+function handleMessage( event ) {
+    switch( event.name ) {
     case 'items':
-        event.message.forEach(function(item){
-            Manager.createItem(item.key, item.data);
-        });
+        event.message.forEach( function( item ) {
+            Manager.createItem( item.key, item.data );
+        } );
         break;
     case 'getItem':
-        Manager.bindEditForm(event.message[0], event.message[1]);
-        break;
-    case 'shouldConvert':
-        if (!localStorage.getItem('converterWasRun')) {
-            styleStorage.each(function(key, data){
-                Manager.setItem(key, data);
-            });
-            localStorage.setItem('converterWasRun', 1);
-            Manager.reloadStyles();
-            document.location = document.location;
-        }
+        Manager.bindEditForm( event.message[0], event.message[1] );
         break;
     }
 }
 
-safari.self.addEventListener("message", handleMessage, false);
-window.addEventListener('DOMContentLoaded', function(){ Manager.start(); }, false);
+safari.self.addEventListener( 'message', handleMessage, false );
+window.addEventListener( 'DOMContentLoaded', Manager.start, false );
--- share/storage.js
+++ share/storage.js
@@ -1,49 +1,27 @@
 var styleStorage = {
 
-    getItem: function(key) {
+    getItem: function( key ) {
         var jsonString = localStorage.getItem(key);
-        if (jsonString) {
-            var data = JSON.parse(jsonString);
-            if (!data.name)
-                data.name = key;
-            return data;
-        }
+        return jsonString ? JSON.parse( jsonString ) : null;
     },
 
-    setItem: function(key, data) {
-        var jsonString = JSON.stringify(data);
-        localStorage.setItem(key, jsonString);
+    setItem: function( key, data ) {
+        localStorage.setItem( key, JSON.stringify( data ) );
         return;
     },
 
-    removeItem: function(key) {
+    removeItem: function( key ) {
         localStorage.removeItem(key);
         return;
     },
 
-    each: function(fn) {
+    each: function( fn ) {
         for (var i = localStorage.length - 1; i >= 0; i--){
-            var key = localStorage.key(i);
-            if (parseInt(key)) {
-                var data = styleStorage.getItem(key);
-                fn(key, data);
+            var key = localStorage.key( i );
+            if( key ) {
+                var data = styleStorage.getItem( key );
+                fn( key, data );
             }
         }
-    },
-
-    makeConsistent: function() {
-        styleStorage.each(function(key, data){
-            // Build 2: enable/disable checkbox.
-            if (data.enabled === undefined) {
-                data.enabled = true;
-                styleStorage.setItem(key, data);
-            }
-            // Build 3: support for exclusions.
-            if (data.excludes === undefined) {
-                data.excludes = [];
-                styleStorage.setItem(key, data);
-            }
-        });
     }
-
 };
